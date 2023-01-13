c9104ce8b335e6a8c3f20d0311d7fc082d7be755
Francesco Aglieco <francesco.aglieco@live.com>
2023-01-13 22:31:19 +0100

    Modified entry point

--- resource/entry_point.sh
+++ resource/entry_point.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-source /home/.bashrc
 export TURTLEBOT3_MODEL=burger
 
-ros2 launch webots_ros2_turtlebot robot_rplidar_launch.py world:=/home/webots_ws/install/webots_ros2_turtlebot/share/webots_ros2_turtlebot/worlds/turtlebot3_rplidar_obstacle.wbt &
-ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=/home/map/map.yaml.yaml &
+ros2 launch webots_ros2_turtlebot robot_rplidar_launch.py world:=src/nav2_dynamic_bringup/worlds/turtlebot3_rplidar_obstacle.wbt &
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=src/nav2_dynamic_bringup/resource/map.yaml.yaml &
 ros2 launch nav2_dynamic_bringup nav2_launch.py &
 
 ros2 topic pub --once /initialpose geometry_msgs/msg/PoseWithCovarianceStamped '{
