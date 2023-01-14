8e8294644b4f0aa6fbadc00108eb36d91e7d9f23
Francesco Aglieco <francesco.aglieco@live.com>
2023-01-14 19:03:51 +0100

    Update world, Dockerfile and ros_entrypoint
    Needed many changes to work out with simulation

--- resource/entry_point.sh
+++ resource/entry_point.sh
@@ -2,8 +2,12 @@ set -e
 
 export TURTLEBOT3_MODEL=burger
 
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+source $OVERLAY_WS/install/local_setup.bash
+
 ros2 launch nav2_dynamic_bringup robot_rplidar_launch.py world:=turtlebot3_rplidar_obstacle.wbt &
-ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$oVERLAY_WS/src/nav2_dynamic_bringup/resource/map.yaml.yaml &
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$OVERLAY_WS/src/nav2_dynamic_bringup/resource/map.yaml.yaml &
 ros2 launch nav2_dynamic_bringup nav2_launch.py &
 
 ros2 topic pub --once /initialpose geometry_msgs/msg/PoseWithCovarianceStamped '{
